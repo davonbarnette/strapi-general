7fd0978dc56ef3dbde3a4e76a96aa8e1812c89ef
Davon Barnette <davonbarnette@gmail.com>
2021-04-19 09:45:35 -0400

    add model, change run script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 docker login &&
 docker pull davonbarnette/general-strapi-api:latest &&
 docker rm -f general-strapi-api
-docker run --env-file general-strapi-api.env -p 1337:1337 davonbarnette/general-strapi-api
+docker run --name general-strapi-api --env-file general-strapi-api.env -p 1337:1337 davonbarnette/general-strapi-api
